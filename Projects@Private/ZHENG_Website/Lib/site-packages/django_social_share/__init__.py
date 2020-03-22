86f4324d0bb4a7089326be20e96143f04ad8a4d6
Zheng Yuen <9320285+ZhengYuen@users.noreply.github.com>
2020-03-22 18:01:52 +0800

    Update

--- Projects@Private/ZHENG_Website/Lib/site-packages/django_social_share/__init__.py
+++
@@ -1,1 +0,0 @@
-VERSION = tuple(map(int, "1.3.2".split('.')))
--- Projects@Private/ZHENG_Website/Projects/ZWeb/Activate Project and Run Server.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\Users\Zheng\Python\ZHENG_Website\Projects\ZWeb"
-call "C:\Users\Zheng\Python\ZHENG_website\Scripts\activate.bat"
-python manage.py runserver
