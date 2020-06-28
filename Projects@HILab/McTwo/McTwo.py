a18b433bfc39a8172be07fb8869198ea662ef98e
Zheng Yuen <9320285+ZhengYuen@users.noreply.github.com>
2020-06-28 21:22:34 +0800

    Add more notes.

--- Projects@HILab/McTwo/McTwo.py
+++ Projects@HILab/McTwo/McTwo.py
@@ -1,6 +1,5 @@
-#Import Libraries
-import os
-import sys
+#%%
+# Import Libraries
 import platform
 import numpy as np
 import pandas as pd
@@ -10,22 +9,28 @@ import sklearn
 from minepy import MINE
 print('-'*100)
 print('Software Information:')
-print('Python Version =',platform.python_version())  
-print('Numpy Version =',np.__version__)
-print('Pandas Version =',pd.__version__)
-print('Minepy Version =',minepy.__version__)
-print('Scipy Version =',sp.__version__)
-print('Scikit-learn Version = ',sklearn.__version__)
+print('Python Version =', platform.python_version())
+print('Numpy Version =', np.__version__)
+print('Pandas Version =', pd.__version__)
+print('Minepy Version =', minepy.__version__)
+print('Scipy Version =', sp.__version__)
+print('Scikit-learn Version = ', sklearn.__version__)
 
 '''----McOne----'''
 print('-'*100)
 print('McOne Started')
-def fLoadDataMatrix(FilePath):
-    with open(FilePath) as file_object:
-        contents = pd.read_csv(FilePath,sep='\t')
-    return (contents)
+
+
+def load_data_matrix(file_path):
+    with open(file_path) as file_object:
+        contents = pd.read_csv(file_path, sep='\t')
+    return contents
+
+#%%
+
+
 def minfo(matrix,name):
-    '''Display Matrix Information, it receives two parameters(matrix,matrix_name)'''
+    # Display Matrix Information, it receives two parameters(matrix,matrix_name)
     print(f'\nMatrix ---*{name}*--- Information')
     try:
         print("Type:",type(matrix))
@@ -42,69 +47,82 @@ def minfo(matrix,name):
     try:
         print("Shape:",matrix.shape)
     except Exception:
-        pass    
+        pass
     try:
         print("Dimission:",matrix.ndim)
     except Exception:
-        pass  
+        pass
     #print(matrix)
     print('-'*40)
-def mic(x,y):
-    '''Calculate the Maximal Information Coffecient'''
+
+
+def mic(x, y):
+    # Calculate the Maximal Information Coefficient
     mine = MINE(alpha=0.6, c=15, est="mic_approx")
-    mine.compute_score(x,y)
-    return(mine.mic())
+    mine.compute_score(x, y)
+    return mine.mic()
 
-#Super Parameters & Load Data
-r = 0.33                                              #r is threshold
-data_set_path = 'Data\ALL3.txt'
-df = fLoadDataMatrix(data_set_path)
-#print('\nVisualize Original DataSet:\n',df)
-#print('-'*100)
 
-#Step 0: Calculate MIC for each gene
-f_array = df.iloc[:,1:].to_numpy()                    #Feature array, converted to numpy array
-c_array = df.columns.str.contains('POS*')==True       
-c_mic = c_array[1:]                                   #Class array, boolen 
-MIC = []                                              #MIC array stores the MIC value for each row(gene)
-f_sel_index = []                                      #The index for selected feature
+# Super Parameters & Load Data
+r = 0.33
+#r is threshold
+data_set_path = 'Data/ALL3.txt'
+df = load_data_matrix(data_set_path)
+# print('\nVisualize Original DataSet:\n',df)
+# print('-'*100)
+
+# Step 0: Calculate MIC for each gene
+f_array = df.iloc[:, 1:].to_numpy()
+# Feature array, converted to numpy array
+c_array = df.columns.str.contains('POS*') == True
+c_mic = c_array[1:]
+# Class array, boole
+
+MIC = []
+# MIC array stores the MIC value for each row(gene)
+f_sel_index = []
+# The index for selected feature
 for i in range(len(df)):
-    f_i = f_array[i,:]
-    mic_i= mic(f_i,c_mic)
+    f_i = f_array[i, :]
+    mic_i = mic(f_i, c_mic)
     MIC.append(mic_i)
     if mic_i > r:
         f_sel_index.append(i)
 print('\nCompute MICs finished.')
-print('\nThe Maixmal MIC =',max(MIC))
+print('\nThe Maixmal MIC =', max(MIC))
 
 '''------Rank Selectred Features using MIC------'''
 #Step 1: Sort f_sel_index in descending order
 df_new = df[['mdr']].copy()
-df_new['MIC_VALUE'] = MIC                           #Create a new DataFrame with only Gene names and its MIC values respectivly.                 
-df_new_selected = df_new.iloc[f_sel_index,:]        #Use the f_sel_index to choose the features from Step 1
-df_new_selected_sorted = df_new_selected.sort_values(by='MIC_VALUE',ascending = False)  #Sort it into Descending Order
-f_sel_index_sorted = df_new_selected_sorted.index   #New index for selected&sorted features
+df_new['MIC_VALUE'] = MIC
+# Create a new DataFrame with only Gene names and its MIC values respectively.
+df_new_selected = df_new.iloc[f_sel_index, :]
+# Use the f_sel_index to choose the features from Step 1
+df_new_selected_sorted = df_new_selected.sort_values(by='MIC_VALUE',ascending = False)
+# Sort it into Descending Order
+f_sel_index_sorted = df_new_selected_sorted.index
+# New index for selected&sorted features
 
-#Step 2: Delete Redundant Features
+# Step 2: Delete Redundant Features
 iter_num = len(f_sel_index_sorted)
 dropped_features_index = []
 for e in range(iter_num):
-    q = e +1
+    q = e + 1
     while q < iter_num:
-        mic_eq = mic(f_array[f_sel_index_sorted[e]],f_array[f_sel_index_sorted[q]])
+        mic_eq = mic(f_array[f_sel_index_sorted[e]], f_array[f_sel_index_sorted[q]])
         mic_q = MIC[f_sel_index[q]]
         if mic_eq >= mic_q:
             df_new_selected_sorted = df_new_selected_sorted.drop(df_new_selected_sorted.index[q]) 
-            #Drop feature indexed as [q in f_sel_index], which is indexed as [f_sel_index[q] in df_new_selected_sorted]
+            # Drop feature indexed as [q in f_sel_index], which is indexed as [f_sel_index[q] in df_new_selected_sorted]
             iter_num -= 1
             dropped_features_index.append(f_sel_index_sorted[q])
-            #print(f'Drop feature with index [{f_sel_index_sorted[q]}].')
+            # print(f'Drop feature with index [{f_sel_index_sorted[q]}].')
             f_sel_index_sorted = f_sel_index_sorted.delete(q)
-            #print(f_sel_index_sorted)
-            #print('New selected feature arrya after droping:\n',df_new_selected_sorted)
+            # print(f_sel_index_sorted)
+            # print('New selected feature arrya after droping:\n',df_new_selected_sorted)
         else:
             q += 1
-print(f'\nFeatures selected by McOne under threshold r = {r} are:\n',df_new_selected_sorted['mdr'])
+print(f'\nFeatures selected by McOne under threshold r = {r} are:\n', df_new_selected_sorted['mdr'])
 print('\nMcOne Finshed')
 print('-'*100)
 '''----END of McOne----'''
@@ -112,17 +130,23 @@ print('-'*100)
 '''----McTwo----'''
 print('-'*100)
 print('McTwo Started')
+
 from sklearn.model_selection import LeaveOneOut
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import balanced_accuracy_score
 
+
 def calculate_BAcc(features_array):
     '''For given feature(s), use NN to calculate its BAcc_values with respect to their true lables. Leave-one-out'''
-    features_array = features_array.T                      #Features subset used to calculate BAcc
-    true_labels = 1*c_mic.T                                #true_labels =  Data set cloumns converted to boolean then 0/1.
+    features_array = features_array.T
+    # Features subset used to calculate BAcc
+    true_labels = 1*c_mic.T
+    # true_labels =  Data set columns converted to boolean then 0/1.
     loo = LeaveOneOut()
-    loo.get_n_splits(features_array)                       #The number of iterations.
-    classifier = KNeighborsClassifier(n_neighbors = 1)     #KNN, k = 1
+    loo.get_n_splits(features_array)
+    # The number of iterations.
+    classifier = KNeighborsClassifier(n_neighbors=1)
+    # KNN, k = 1
     y_pred = []
     for train_index, test_index in loo.split(features_array):
         X_train, X_test = features_array.iloc[train_index], features_array.iloc[test_index]
@@ -131,7 +155,9 @@ def calculate_BAcc(features_array):
         y_pred_i = classifier.predict(X_test)
         y_pred.append(y_pred_i)
     BAcc = balanced_accuracy_score(true_labels, y_pred)
-    return(BAcc)
+    return BAcc
+
+
 def return_selected_features(selected_feature_index_array):
     '''Function name should be different from selected_feature(pd.Dataframe)'''
     try:
@@ -140,10 +166,11 @@ def return_selected_features(selected_feature_index_array):
         #Convert series into dataframe then transpose.
     except Exception:
         selected_features = df.iloc[selected_feature_index_array,1:]
-    return(selected_features)
+    return selected_features
+
 
 '''Best-first-search'''
-all_features = df.iloc[f_sel_index_sorted,1:]
+all_features = df.iloc[f_sel_index_sorted, 1:]
 selected_features_index = []                                                        #Only Manipulate the index
 selected_features = return_selected_features(selected_features_index)
 
@@ -157,7 +184,7 @@ selected_features_index.append(all_features.index[np.argmax(BAcc_array)])
 
 #Step 1: The rest iterations
 flag = True
-while flag == True:
+while flag:
     left_features = all_features.drop(selected_features.index)
     BAcc_array = []
     for j in range(len(left_features)):
@@ -171,14 +198,14 @@ while flag == True:
             Max_BAcc = test_included_bacc
             selected_features_index = temp_combination.index
             print('\nNew Improvement')
-            print('Temporary maximal BAcc: ',Max_BAcc)
+            print('Temporary maximal BAcc: ', Max_BAcc)
             #print('Indices for selected features',selected_features_index)
         #print('-'*40)
     if Max_BAcc > max(BAcc_array):
         '''Algorithm stops if Max_BAcc stops increasing.'''
         flag = False
-print('\nFeatures selected by McTwo are: \n',df.iloc[return_selected_features(selected_features_index).index]['mdr'])
-print('\nThe best BAcc: ',Max_BAcc)
-print('\nMcTwo Finshed')
+print('\nFeatures selected by McTwo are: \n', df.iloc[return_selected_features(selected_features_index).index]['mdr'])
+print('\nThe best BAcc: ', Max_BAcc)
+print('\nMcTwo Finished')
 print('-'*100)
 '''----END of McTwo----'''
