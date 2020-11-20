6c82172c574a40a6ee2575ddc7798a51a103c681
Zheng Yuen <9320285+ZhengYuen@users.noreply.github.com>
2020-11-20 22:26:22 +0800

    Reorganized folder structure

--- Computer Science/LeetCode/561.Array_Partition_I.py
+++
@@ -1,8 +0,0 @@
-array = [1,2,4,5,6,7,555,4698]
-array.sort()
-INPUT = array
-sum = 0
-for i in range(int(len(INPUT)/2)):
-    num_i = INPUT[2*i]
-    sum += num_i
-print(sum)
--- Projects@HILab/EEG/Data(OpenSource)/visualize_data.m
+++
@@ -1,4 +0,0 @@
-N1 = load('N1.mat');
-data = N1.dataStruct.data;
-channels = N1.dataStruct.channelIndices;
-figure, plot(data(:,channels(5)))
